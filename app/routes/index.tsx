c6ad6837028ddf6493ecf4fabcac86e80363abce
Roj <ez@roj.im>
2022-12-24 18:16:36 +0300

    Improve button icon size

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
       <main className="w-full max-w-screen-xl mx-auto p-2 flex flex-col items-center">
         <div>
           <button className="rounded-full px-4 py-2 bg-[rgba(0,0,0,0.05)] dark:bg-[rgba(255,255,255,0.125)] hover:bg-[rgba(0,0,0,0.1)] dark:hover:bg-[rgba(255,255,255,0.1875)] flex items-center gap-1.5">
-            <FaMicrophone size={20} /> دەست بکە بە تۆمارکردن
+            <FaMicrophone size={18} /> دەست بکە بە تۆمارکردن
           </button>
         </div>
       </main>
