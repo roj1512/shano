1c699ffbb63663816d06d3352389e02387e60916
Roj <ez@roj.im>
2022-12-24 18:03:01 +0300

    Initialize header and record button

---
+++ app/logos/Shano.tsx
@@ -0,0 +1,17 @@
+export default function Shano() {
+  return (
+    <svg
+      viewBox="0 0 100 100"
+      width="25"
+      height="25"
+      className="fill-black dark:fill-white"
+    >
+      <path
+        fillRule="evenodd"
+        d="M50 90c22.091 0 40-17.909 40-40S72.091 10 50 10 10 27.909 10 50s17.909 40 40 40Zm0 10c27.614 0 50-22.386 50-50S77.614 0 50 0 0 22.386 0 50s22.386 50 50 50Z"
+        clipRule="evenodd"
+      />
+      <path d="M75 50c0 13.807-11.193 25-25 25S25 63.807 25 50s11.193-25 25-25 25 11.193 25 25Z" />
+    </svg>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,21 @@
+import { FaMicrophone } from "react-icons/fa";
+import Shano from "~/logos/Shano";
+
 export default function Index() {
   return (
     <>
+      <header className="w-full max-w-screen-xl mx-auto p-2">
+        <button className="rounded-full px-4 py-2 hover:bg-[rgba(0,0,0,0.05)] dark:hover:bg-[rgba(255,255,255,0.125)] flex font-bold items-center gap-1.5 text-lg">
+          <Shano /> شانۆ
+        </button>
+      </header>
+      <main className="w-full max-w-screen-xl mx-auto p-2 flex flex-col items-center">
+        <div>
+          <button className="rounded-full px-4 py-2 bg-[rgba(0,0,0,0.05)] dark:bg-[rgba(255,255,255,0.125)] hover:bg-[rgba(0,0,0,0.1)] dark:hover:bg-[rgba(255,255,255,0.1875)] flex items-center gap-1.5">
+            <FaMicrophone size={20} /> دەست بکە بە تۆمارکردن
+          </button>
+        </div>
+      </main>
     </>
   );
 }
