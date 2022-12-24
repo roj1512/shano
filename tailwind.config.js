a07a637dd64ab91e0b1168ce8d96a3a861479da8
Roj <ez@roj.im>
2022-12-24 16:25:49 +0300

    Initialize Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        shano: "#1E90FF",
+      },
+    },
+  },
+  plugins: [],
+};
