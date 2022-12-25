33d18def84e6b189953dbdfc43a7d9f934d3b183
Roj <ez@roj.im>
2022-12-25 18:46:14 +0300

    Add link to repository to the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,18 @@
 export default function Footer() {
   return (
     <footer className="p-3 border-t-2 border-[rgba(0,0,0,.05)] dark:border-[rgba(255,255,255,.0375)]">
-      <div className="opacity-50 text-xs w-full mx-auto max-w-3xl">
-        <p>&copy; 2022</p>
+      <div className="text-xs w-full mx-auto max-w-3xl gap-2 flex justify-between">
+        <p className="opacity-50">
+          &copy; 2022
+        </p>
+        <a
+          href="https://github.com/roj1512/shano"
+          className="text-shano hover:opacity-50 duration-100"
+          target="_blank"
+          rel="noreferrer"
+        >
+          سەرچاوە
+        </a>
       </div>
     </footer>
   );
